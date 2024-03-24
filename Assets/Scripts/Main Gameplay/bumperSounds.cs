e47c0b94a84714a13fba88c05b828fb4bcf105d1
cannedShrimp614 <164089702+cannedShrimp614@users.noreply.github.com>
2024-03-23 21:27:19 -0400

    Push

--- Assets/Scripts/Main Gameplay/bumperSounds.cs
+++ Assets/Scripts/Main Gameplay/bumperSounds.cs
@@ -13,23 +13,7 @@ public class bumperSounds : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D otherObject)
     {
-        if (otherObject.gameObject.CompareTag("1Pointer"))
-        {
-            myAudio.Play();
-        }
-        if (otherObject.gameObject.CompareTag("5Pointer"))
-        {
-            myAudio.Play();
-        }
-        if (otherObject.gameObject.CompareTag("10Pointer"))
-        {
-            myAudio.Play();
-        }
-        if (otherObject.gameObject.CompareTag("20Pointer"))
-        {
-            myAudio.Play();
-        }
-        if (otherObject.gameObject.CompareTag("50Pointer"))
+        if (otherObject.gameObject.CompareTag("1Pointer") || otherObject.gameObject.CompareTag("50Pointer") || otherObject.gameObject.CompareTag("20Pointer") || otherObject.gameObject.CompareTag("10Pointer")|| otherObject.gameObject.CompareTag("5Pointer"))
         {
             myAudio.Play();
         }
